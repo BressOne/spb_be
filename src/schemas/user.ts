fbb7a8cc3aaae059f9804f14a16011c604c18322
BressOne <1992bress@gmail.com>
2023-09-22 00:46:30 +0300

    fix user schema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -4,7 +4,7 @@ import { v4 } from 'uuid';
 const UserSchema = new Schema({
   id: { type: String, unique: true, required: true, default: v4() },
   restaurantOrigin: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
